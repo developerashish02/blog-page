7f4525c945ee3b87e1550db68f50ee65508a33e1
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-04-01 18:11:29 +0530

    implementing Navbar component

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom'; 
 function Navbar() {
     return (
         <div>
-            Navbar
+            <ul id='nav'>
+                <li>
+                    <Link to="/" >Home</Link>
+                    <Link to="/create-post" > Create Post</Link>
+                </li>
+            </ul>
         </div>
     )
 }
